c4c74c912876c86455cf1cf87db35125cc36dc5c
unknown <sheelajagadish29@gmail git config --global user.name sheelajagadish29.com>
2021-03-16 10:41:36 +0530

    credit card project

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+$(function() {
+
+    var owner = $('#owner');
+    var cardNumber = $('#cardNumber');
+    var cardNumberField = $('#card-number-field');
+    var CVV = $("#cvv");
+    var mastercard = $("#mastercard");
+    var confirmButton = $('#confirm-purchase');
+    var visa = $("#visa");
+
+
+    cardNumber.payform('formatCardNumber');
+    CVV.payform('formatCardCVC');
+
+
+    cardNumber.keyup(function() {
+
+        
+        visa.removeClass('transparent');
+        mastercard.removeClass('transparent');
+
+        if ($.payform.validateCardNumber(cardNumber.val()) == false) {
+            cardNumberField.addClass('has-error');
+        } else {
+            cardNumberField.removeClass('has-error');
+            cardNumberField.addClass('has-success');
+        }
+
+        if ($.payform.parseCardType(cardNumber.val()) == 'visa') {
+            mastercard.addClass('transparent');
+           
+        }  else if ($.payform.parseCardType(cardNumber.val()) == 'mastercard') {
+            
+            visa.addClass('transparent');
+        }
+    });
+
+    confirmButton.click(function(e) {
+
+        e.preventDefault();
+
+        var isCardValid = $.payform.validateCardNumber(cardNumber.val());
+        var isCvvValid = $.payform.validateCardCVC(CVV.val());
+
+        if(owner.val().length < 3){
+            alert("Wrong owner name");
+        } else if (!isCardValid) {
+            alert("Wrong card number");
+        } else if (!isCvvValid) {
+            alert("Wrong CVV");
+        } else {
+            alert("Everything is correct");
+        }
+    });
+});
